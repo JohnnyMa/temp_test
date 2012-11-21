e7546e08591baa4325b8b50986d8920e9a482789
johnnyma <johnnyma1988@gmail.com>
2012-11-21 14:10:47 +0800

    refine requestHandler

--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -1,5 +1,12 @@
 function start() {
   console.log("Request handler 'start' was called.");
+
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime < startTime + milliSeconds);
+  }
+
+  sleep(10000);
   return "Hello Start.";
 }
 
@@ -9,4 +16,4 @@ function upload() {
 }
 
 exports.start = start;
-exports.upload = upload;
+exports.upload = upload; 
